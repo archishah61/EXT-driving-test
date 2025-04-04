bfd81355090f13ae650247eea753e6d0eccae65b
i35Excelsior <i3.5.excelsior@gmail.com>
2025-04-04 15:14:17 +0530

    app router set up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomeLayout from "./component/HomeLayout/HomeLayout";
+
+import Home from "./component/Home/Home";
+import AboutUs from "./component/Home/About";
+import ContactUs from "./component/Home/Contact";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/about", element: <AboutUs /> },
+      { path: "/contact", element: <ContactUs /> },
+    ],
+  },
+]);
 
 export default function App() {
-  return (
-    <div>App</div>
-  )
+  return <RouterProvider router={router} />;
 }
