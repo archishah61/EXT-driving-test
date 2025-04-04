b0604a357d0c635adaa8226e2738fb0a76202f04
archishah61 <archishah61@gmail.com>
2025-04-04 19:52:47 +0530

    update testtabs

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../../component/HomeComponents/Hero'
 import TestTabs from '../../component/HomeComponents/TestTabs'
+import HowItWorks from '../../component/HomeComponents/HowItWorks'
 export default function Home() {
   return (
     <>
       <Hero />
       <TestTabs/>
+      <HowItWorks/>
     </>
   )
 }
